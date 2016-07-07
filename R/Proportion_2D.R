62bf3ab3ae4bb48157f88ab6e39e6cbc01f28baa
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2016-07-07 15:26:45 +0100

    added proportions with non assigned values

---
+++ R/Proportion_2D.R
@@ -0,0 +1,13 @@
+#' Compute the proportions from a set of 2D images describing facies
+#' The proportion is not straightforward because some values can be non assigned
+#' Therefore all the facies are required and a correction is applied for the no value cells
+
+Proportion_2D<-function(images){
+  nfacies <- length(images)
+  facies <- reading_facies_images(images)
+  prop<-rep(0,nfacies)
+  for( i in seq(facies)){
+    prop[i]=mean(facies[[i]])
+  }
+
+}
