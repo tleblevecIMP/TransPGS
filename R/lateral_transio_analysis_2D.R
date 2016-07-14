5b75e28ffa7dde1e5aa7aa46031495bb97ac4cb0
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2016-07-14 16:57:54 +0100

    solved problem with facies called

--- R/lateral_transio_analysis_2D.R
+++ R/lateral_transio_analysis_2D.R
@@ -4,6 +4,6 @@
 lateral_transio_analysis_2D<-function(images,length,transio_length){
   windows()
   facies <- reading_facies_images(images)
-  mat_t<-transio_matrix(length(images),facies,length,ncol(facies[[1]]),transio_length,0)
+  mat_t<-transio_matrix(length(images),facies,length,ncol(facies),transio_length,0)
   return(mat_t)
 }
