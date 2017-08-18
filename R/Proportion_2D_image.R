eda4c3598a4e50d020d8dae1705a112797eaadcc
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2017-08-18 14:52:09 +0100

    putted the two proportion functions in one file

--- R/Proportion_2D_image.R
+++ R/Proportion_2D_image.R
@@ -9,3 +9,17 @@ Proportion_2D_image<-function(images){
   prop <- proportion_2D(facies,nfacies)
   return ( prop)
 }
+
+#'Compute the proportions of a 2d dataset coded in a list of matrix
+#'
+
+
+proportion_2D<-function(facies,nfacies){
+  prop<-rep(0,nfacies)
+  for( i in seq(nfacies)){
+    fi<-binarize_facies(facies,i)
+    prop[i]=mean(fi,na.rm=TRUE)
+  }
+  #prop<-prop_correction_NA(prop)
+  return ( prop)
+}
