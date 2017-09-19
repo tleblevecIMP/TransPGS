57d85adba583fb8128cc803ad937b808a1506732
tleblevecIMP <t.le-blevec15@imperial.ac.uk>
2017-09-19 17:54:08 +0100

    study embedded transitions on latemar

--- main.R
+++ main.R
@@ -1,5 +1,11 @@
 
 
+# test one one log of the latemar
+facies<-reading_facies_logs("latemar_section.txt",5)
+props<-proportion_2D(facies,5)
+t<-transio_analysis_log("latemar_section.txt",5,10,3)
+
+# other test
 #infering model
 facies<-reading_facies_logs("latemar60m.csv",3)
 props<-proportion_2D(facies,5)
@@ -24,8 +30,37 @@ nwell=ncol(facies)
 
 # preparing the grid
 # the vector is the vector of locations of the different wells (supposed all vertical)
-data_location<-matrix(c(1,1,5,10),nwell,2)
+data_location<-matrix(c(1,1,5,10),nwell,2,byrow = TRUE)
 grid<-grid_construct(nx,ny,facies,data_location)
 
 #sim
 sim<-condsim3d(props,grid,r1v,r1hx,r1hy,f,r2v,r2hx,r2hy,f2,rho,shift,N,dz,dx,dy,nz,nx,ny)
+
+
+# test on Martin paper
+
+rf1<-reading_facies_logs("test2.txt",3)
+prps<-proportion_2D(rf1,3)
+transio<-transio_analysis_log("test2.txt",3,141,50)
+library(mvtnorm)
+gaussian_transio_interact(prps,transio,1,1,100,100)
+facies<-rf1
+nx=6
+ny=4
+nz=nrow(facies)
+dz=1
+dx=100
+dy=100
+N=10
+f=f2=0
+r1v=0.5
+r2v=3.43
+r1hx=r1hy=2.7*0.5
+r2hx=r2hy=3.43*1.74
+rho=0.48
+shift=0
+nwell=ncol(facies)
+facies<-rf1
+data_location<-matrix(c(3,1,4,1,5,1,6,1,1,2,2,1,6,4),nwell,2,byrow = TRUE)
+grid<-grid_construct(nx,ny,rf1,data_location)
+sim<-condsim3d(prps,grid,r1v,r1hx,r1hy,f,r2v,r2hx,r2hy,f2,rho,shift,N,dz,dx,dy,nz,nx,ny)
